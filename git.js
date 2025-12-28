760788be681fdc138687514ee54a4393d6d32fc5
6oox <njiin@hotmail.com>
2025-12-28 09:52:46 -0300

    Add files via upload

---
+++ git.js
@@ -0,0 +1,101 @@
+COMANDOS DO TERMUX🎖️
+
+
+EVIAR BOT PARA GIT
+
+
+1° Comando
+
+cd /sdcard/pasta-do-bot
+
+2° Comando
+
+rm -rf .git
+
+3° Comando
+
+git init
+
+4° Comando
+
+git config --global --add safe.directory /storage/emulated/0/ PASTA DO BOT
+
+5° Comando
+
+git add .
+
+
+6° Comando
+
+git config --global user.email ""
+
+7° Comando
+
+git config --global user.name "⠀"
+
+git config --global user.name "6oox"
+
+8° Comando
+
+git commit -m "First commit"
+
+9° Comando
+
+git remote add origin "link"
+
+10° Comando
+
+git remote -v
+
+11° Comando
+
+git push origin master
+
+Nome De Usuário: "6oox"
+
+Token: ""
+
+__________________________
+
+
+1° Comando
+
+cd /sdcard/pasta-do-bot
+
+2° Comando
+
+git add .
+
+3° Comando
+
+git commit -m "First commit"
+
+4° Comando
+
+git push origin master
+
+FIM!!!
+
+
+
+
+se ATUALIZAR GIT
+
+
+.
+1° Comando
+
+cd /sdcard/pasta-do-bot
+
+
+2° Comando
+
+git add .
+
+3° Comando
+
+git commit -m "First commit"
+
+4° Comando
+
+git push origin master
